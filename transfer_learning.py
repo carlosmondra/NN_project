59bbd623e83046a5a162d50527323618cb83aafc
Carlos Mondragon <mondragon.chapa.carlos@gmail.com>
2018-12-07 17:23:13 -0500

    Remove some unused code

--- transfer_learning.py
+++ transfer_learning.py
@@ -135,62 +135,6 @@ def train():
         print("Finish epoch {}, time elapsed {}".format(epoch, time.time() - ts))
         torch.save(fcn_model, model_path)
 
-        # val(epoch)
-
-
-# def val(epoch):
-#     fcn_model.eval()
-#     total_ious = []
-#     pixel_accs = []
-#     for iter, batch in enumerate(val_loader):
-#         if use_gpu:
-#             inputs = Variable(batch['X'].cuda())
-#         else:
-#             inputs = Variable(batch['X'])
-
-#         output = fcn_model(inputs)
-#         output = output.data.cpu().numpy()
-
-#         N, _, h, w = output.shape
-#         pred = output.transpose(0, 2, 3, 1).reshape(-1, n_class).argmax(axis=1).reshape(N, h, w)
-
-#         target = batch['l'].cpu().numpy().reshape(N, h, w)
-#         for p, t in zip(pred, target):
-#             total_ious.append(iou(p, t))
-#             pixel_accs.append(pixel_acc(p, t))
-
-#     # Calculate average IoU
-#     total_ious = np.array(total_ious).T  # n_class * val_len
-#     ious = np.nanmean(total_ious, axis=1)
-#     pixel_accs = np.array(pixel_accs).mean()
-#     print("epoch{}, pix_acc: {}, meanIoU: {}, IoUs: {}".format(epoch, pixel_accs, np.nanmean(ious), ious))
-#     IU_scores[epoch] = ious
-#     np.save(os.path.join(score_dir, "meanIU"), IU_scores)
-#     pixel_scores[epoch] = pixel_accs
-#     np.save(os.path.join(score_dir, "meanPixel"), pixel_scores)
-
-
-# # borrow functions and modify it from https://github.com/Kaixhin/FCN-semantic-segmentation/blob/master/main.py
-# # Calculates class intersections over unions
-# def iou(pred, target):
-#     ious = []
-#     for cls in range(n_class):
-#         pred_inds = pred == cls
-#         target_inds = target == cls
-#         intersection = pred_inds[target_inds].sum()
-#         union = pred_inds.sum() + target_inds.sum() - intersection
-#         if union == 0:
-#             ious.append(float('nan'))  # if there is no ground truth, do not include in evaluation
-#         else:
-#             ious.append(float(intersection) / max(union, 1))
-#         # print("cls", cls, pred_inds.sum(), target_inds.sum(), intersection, float(intersection) / max(union, 1))
-#     return ious
-
-
-# def pixel_acc(pred, target):
-#     correct = (pred == target).sum()
-#     total   = (target == target).sum()
-#     return correct / total
 
 def show_img(pt_tensor):
     y_pred_np = pt_tensor.cpu().detach().numpy()
