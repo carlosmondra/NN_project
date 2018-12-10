f1cd758e3197e1bd76915caf18051713486a5d00
Carlos Mondragon <mondragon.chapa.carlos@gmail.com>
2018-12-10 13:19:40 -0500

    Predict traffic refactored

--- predict_traffic.py
+++ predict_traffic.py
@@ -1,54 +1,6 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-
-import torchvision.transforms as transforms
-import PIL.Image
 from PIL import Image
-
-from fully_conv_nets import VGGNet, FCNs#, FCN32s, FCN16s, FCN8s
-
 import numpy as np
-import time
-import sys
 import os
-
-use_gpu = torch.cuda.is_available()
-
-class TensorDataset(torch.utils.data.Dataset):
-    def __init__(self,raw_imgs_dir,masks_dir,data_type,transform=transforms.ToTensor()):
-        """
-        Args:
-        raw_imgs_dir(string): directory with all train images
-        """
-        self.raw_imgs_dir = raw_imgs_dir
-        self.masks_dir = masks_dir
-        self.transform = transform
-        self.data_type = 0
-        if data_type == "cars":
-            self.data_type = 2
-
-    def __len__(self):
-        return len([name for name in os.listdir(self.raw_imgs_dir) if os.path.isfile(os.path.join(self.raw_imgs_dir, name))])
-
-    def __getitem__(self, idx):
-        str_idx = str(idx + 1)
-        img_name = ('0' * (7 - len(str_idx) + 1)) + str_idx + '.png'
-        img_path = os.path.join(self.raw_imgs_dir, img_name)
-        image = PIL.Image.open(img_path).convert('RGB')
-        image = self.transform(image).type(torch.cuda.FloatTensor)
-        
-        #resides in 'NN_project/dataset/car_masks'
-        mask_path = os.path.join(self.masks_dir, img_name)
-        mask = PIL.Image.open(mask_path).convert('RGB')
-        mask = transforms.ToTensor()(mask).type(torch.cuda.LongTensor)
-        labels = mask[self.data_type]
-        # sample = (image, labels)
-        sample = {'X':image, 'Y':labels}
-        return sample
 
 def show_masked_img(roads_tensor, cars_tensor, raw_img):
     # Convert to numpy
